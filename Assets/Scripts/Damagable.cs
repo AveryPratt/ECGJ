12bf81f2ffdc5bcce3d28e171b80c0fd02946555
Avery Pratt <apratt91@gmail.com>
2018-12-14 01:24:00 -0800

    started very basic prototyping

---
+++ Assets/Scripts/Damagable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Damagable : MonoBehaviour
+{
+    public Collider2D Collider { get; private set; }
+
+    private void Start()
+    {
+        Collider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Damaging")
+        {
+            GetHit();
+        }
+    }
+
+    private void GetHit()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Shootable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class Shootable : MonoBehaviour
+{
+    public Rigidbody2D RBody { get; private set; }
+    public Collider2D Collider { get; private set; }
+    public float Speed;
+
+    private void Start()
+    {
+        RBody = GetComponent<Rigidbody2D>();
+        Collider = GetComponent<Collider2D>();
+        RBody.velocity = transform.rotation * new Vector2(0, Speed);
+    }
+}
