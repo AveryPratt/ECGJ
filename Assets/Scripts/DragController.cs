7a4329cdb2357440a77b48319d491488d36d2486
Avery Pratt <apratt91@gmail.com>
2018-12-15 18:45:52 -0800

    made shots dragable

---
+++ Assets/Scripts/DragController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragController : MonoBehaviour
+{
+    public Dragable Target { get; set; }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            Drag();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Target = null;
+        }
+    }
+
+    private void Drag()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Target != null)
+        {
+            if (Physics.Raycast(ray, out hit, 100, LayerMask.GetMask("HitScreen")))
+            {
+                Target.transform.position = hit.point;
+            }
+            else
+            {
+                Debug.Log("Mouse is off HitScreen");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Dragable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragable : MonoBehaviour
+{
+    private DragController Controller;
+
+    private void Start()
+    {
+        Controller = FindObjectOfType<DragController>();
+    }
+
+    private void OnMouseDown()
+    {
+        Controller.Target = this;
+    }
+}
