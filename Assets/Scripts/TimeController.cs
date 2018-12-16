6f265686e592670b3fd343c9459ad66012ccc6af
Avery Pratt <apratt91@gmail.com>
2018-12-15 17:14:58 -0800

    added time change mechanics

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public float CooldownDuration = 1;
+    private float cooldown;
+
+    private void Update()
+    {
+        if (cooldown > 0)
+        {
+            cooldown -= Time.unscaledDeltaTime;
+            if (cooldown < 0)
+            {
+                cooldown = 0;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryStopTime();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            TryResumeTime();
+        }
+    }
+
+    private void TryStopTime()
+    {
+        if (Time.timeScale > 0 && cooldown <= 0)
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    private void TryResumeTime()
+    {
+        if (Time.timeScale < 1)
+        {
+            Time.timeScale = 1;
+            cooldown = CooldownDuration;
+        }
+    }
+}
