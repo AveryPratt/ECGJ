098a29b17dfd4f914f1932773be0f81ee4b5e130
Avery Pratt <apratt91@gmail.com>
2018-12-17 02:22:22 -0800

    added brick sprite and randomized wall rotation

---
+++ Assets/RandomRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    private void Start()
+    {
+        int val = Mathf.FloorToInt(Random.value * 4);
+        transform.Rotate(transform.forward, val * 90);
+    }
+}
